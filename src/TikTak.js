accf057f225262a1e65cbd089a7731dc9dffb2ac
tikesh <tikeshisverma@gmail.com>
2021-02-10 15:18:26 +0530

    feat:styleing done

--- src/TikTak.js
+++ src/TikTak.js
@@ -1,6 +1,34 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes, faCheck } from "@fortawesome/free-solid-svg-icons";
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
+
 import { useEffect, useState } from "react";
 import "./TikTak.css";
-
+const BoxIcon = (props) => {
+  if (props.icon == "x") {
+    return <FontAwesomeIcon icon={faTimes} />;
+  } else if (props.icon == "o") {
+    return <FontAwesomeIcon icon={faCheck} />;
+  } else {
+    return null;
+  }
+};
+const WinnerBox =(props)=>{
+  const {winner} = props
+  const { width, height } = useWindowSize()
+  return(
+    <div>
+  <Confetti
+    width={width}
+    height={height}/>
+<div>WinneR {winner}</div>
+  </div> 
+ 
+  )
+  
+  
+}
 function TikTak() {
   const [TextContent, setTextContent] = useState([
     "",
@@ -13,17 +41,23 @@ function TikTak() {
     "",
     "",
   ]);
+  const [winner,setWinner]= useState("")
+
   const [turn, setTurn] = useState("playerOne");
 
   const trigger = (n) => {
-    console.log("n==>", n);
+    if(TextContent[n]!=="" || winner !== ""){
+      return
+    }
     let temp = [...TextContent];
     if (turn === "playerOne") {
-      temp[n - 1] = "X";
+      temp[n] = "o";    setTextContent(temp);
+      
     } else if (turn === "playerTwo") {
-      temp[n - 1] = "O";
+      temp[n] = "x";    setTextContent(temp);
     }
-    setTextContent(temp);
+    
+
   };
   useEffect(() => {
     let temp = [...TextContent];
@@ -34,7 +68,7 @@ function TikTak() {
       temp[0] === temp[1] &&
       temp[0] === temp[2]
     ) {
-      alert(`WINNER ${turn}`);
+      setWinner(turn)
     } else if (
       temp[3] !== "" &&
       temp[4] !== "" &&
@@ -42,7 +76,7 @@ function TikTak() {
       temp[3] === temp[4] &&
       temp[3] === temp[5]
     ) {
-      alert(`WINNER ${turn}`);
+      setWinner(turn)
     } else if (
       temp[6] !== "" &&
       temp[7] !== "" &&
@@ -50,7 +84,7 @@ function TikTak() {
       temp[6] === temp[7] &&
       temp[6] === temp[8]
     ) {
-      alert(`WINNER ${turn}`);
+      setWinner(turn)
     } else if (
       temp[0] !== "" &&
       temp[3] !== "" &&
@@ -58,7 +92,7 @@ function TikTak() {
       temp[0] === temp[3] &&
       temp[0] === temp[6]
     ) {
-      alert(`WINNER ${turn}`);
+      setWinner(turn)
     } else if (
       temp[1] !== "" &&
       temp[4] !== "" &&
@@ -66,7 +100,7 @@ function TikTak() {
       temp[1] === temp[4] &&
       temp[1] === temp[7]
     ) {
-      alert(`WINNER ${turn}`);
+      setWinner(turn)
     } else if (
       temp[2] !== "" &&
       temp[5] !== "" &&
@@ -74,7 +108,7 @@ function TikTak() {
       temp[2] === temp[5] &&
       temp[2] === temp[8]
     ) {
-      alert(`WINNER ${turn}`);
+      setWinner(turn)
     } else if (
       temp[0] !== "" &&
       temp[4] !== "" &&
@@ -82,7 +116,7 @@ function TikTak() {
       temp[0] === temp[4] &&
       temp[0] === temp[8]
     ) {
-      alert(`WINNER ${turn}`);
+      setWinner(turn)
     } else if (
       temp[2] !== "" &&
       temp[4] !== "" &&
@@ -90,7 +124,7 @@ function TikTak() {
       temp[2] === temp[4] &&
       temp[2] === temp[6]
     ) {
-      alert(`WINNER ${turn}`);
+      setWinner(turn)
     } else {
       turn === "playerTwo" ? setTurn("playerOne") : setTurn("playerTwo");
     }
@@ -98,20 +132,40 @@ function TikTak() {
 
   return (
     <div className="button-wrapper">
+ {winner ?   <WinnerBox winner={winner}/>  : null}
       <div className="wrapper">
-        <button onClick={() => trigger(1)}>{TextContent[0]}</button>
-        <button onClick={() => trigger(2)}>{TextContent[1]}</button>
-        <button onClick={() => trigger(3)}>{TextContent[2]}</button>
+        <button className="button1" onClick={() => trigger(0)}>
+          <BoxIcon icon={TextContent[0]}></BoxIcon>
+        </button>
+
+        <button className="button2" onClick={() => trigger(1)}>
+          <BoxIcon icon={TextContent[1]}></BoxIcon>
+        </button>
+        <button className="button3" onClick={() => trigger(2)}>
+          <BoxIcon icon={TextContent[2]}></BoxIcon>
+        </button>
       </div>
       <div className="wrapper">
-        <button onClick={() => trigger(4)}>{TextContent[3]}</button>
-        <button onClick={() => trigger(5)}>{TextContent[4]}</button>
-        <button onClick={() => trigger(6)}>{TextContent[5]}</button>
+        <button className="button4" onClick={() => trigger(3)}>
+          <BoxIcon icon={TextContent[3]}></BoxIcon>
+        </button>
+        <button className="button5" onClick={() => trigger(4)}>
+          <BoxIcon icon={TextContent[4]}></BoxIcon>
+        </button>
+        <button className="button6" onClick={() => trigger(5)}>
+          <BoxIcon icon={TextContent[5]}></BoxIcon>
+        </button>
       </div>
       <div className="wrapper">
-        <button onClick={() => trigger(7)}>{TextContent[6]}</button>
-        <button onClick={() => trigger(8)}>{TextContent[7]}</button>
-        <button onClick={() => trigger(9)}>{TextContent[8]}</button>
+        <button  className="button7"onClick={() => trigger(6)}>
+          <BoxIcon icon={TextContent[6]}></BoxIcon>
+        </button>
+        <button className="button8" onClick={() => trigger(7)}>
+          <BoxIcon icon={TextContent[7]}></BoxIcon>
+        </button>
+        <button className="button9" onClick={() => trigger(8)}>
+          <BoxIcon icon={TextContent[8]}></BoxIcon>
+        </button>
       </div>
     </div>
   );
